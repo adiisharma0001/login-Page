0ba1b88ca9d371f54a5fc64075d97b20c9c048c4
Aditya <95631458+Sharmaaditya05@users.noreply.github.com>
2023-04-06 21:48:23 +0530

    first commit

---
+++ login.js
@@ -0,0 +1,22 @@
+
+
+function loginFunc(e) {
+  e.preventDefault();
+
+  var username = document.getElementById('username').value;
+  var pass = document.getElementById('password').value;
+  var result = document.getElementById('result');
+
+  var username = localStorage.getItem(username);
+  var data = JSON.parse(user);
+  console.log(data);
+
+  if (user == null) {
+    result.innerHTML = 'wrong username';
+  } else if (username == data.username && pass == data.password) {
+    result.innerHTML = 'logged in';
+  } else {
+    result.innerHTML = 'wrong password';
+  }
+
+}
